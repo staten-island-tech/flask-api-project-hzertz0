3c26a9e27e0c9db4bab2e73143b02357501d1d4b
hzertz0 <darwenzhu1221@gmail.com>
2025-04-28 10:16:08 -0400

    crashed

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
+# Load the item list once globally
+ITEM_LIST_URL = "https://raw.githubusercontent.com/anish-shanbhag/minecraft-api/master/data/items.json"
+
+def fetch_items():
+    response = requests.get(ITEM_LIST_URL)
+    return response.json()
+
 @app.route("/")
 def index():
-    # Get Minecraft blocks from the API
-    response = requests.get("https://raw.githubusercontent.com/anish-shanbhag/minecraft-api/master/data/items.json")
-    item_list = response.json()  # This is already a list of block dicts
-
+    item_list = fetch_items()
     items = []
     for item in item_list:
         items.append({
             'name': item['name'],
-            'image': item['image']
+            'image': item['image'],
+            'id': item.get('namespacedId', item['name'])  # Use a fallback if namespacedId is missing
         })
-
-    # Pass the blocks to your index.html template
     return render_template("index.html", items=items)
+
+@app.route("/details/<item_id>")
+def item_detail(item_id):
+    item_list = fetch_items()
+    item = next((i for i in item_list if i.get('namespacedId', i['name']) == item_id), None)
+    if item is None:
+        return "Item not found", 404
+    return render_template("details.html", item=item)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
